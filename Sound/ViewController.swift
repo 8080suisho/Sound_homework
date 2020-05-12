9f062abd1ff43cf9986eebeba0fc123779741998
suisho <8080suisho@gmail.com>
2020-05-12 18:34:01 +0900

    homework Sound commit 2回目

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet var drumButton: UIButton!
 
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drum_sound")!.data)
     
@@ -20,9 +21,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        drumButton.setImage(UIImage(named: "drum_playing_image"), for: .normal)
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drum_image"), for: .normal)
     }
 
 
